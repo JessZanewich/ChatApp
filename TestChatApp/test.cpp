adfbcf96cc88b9eb53300d9813b019ad00af57a7
MostlyArmless <mike.j.thiem@gmail.com>
2019-04-13 22:03:28 -0600

    Added skeleton code for test project.

--- TestChatApp/test.cpp
+++ TestChatApp/test.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
+#include "../BackEnd/Server.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class ServerTest : public ::testing::Test {
+protected:
+	std::unique_ptr<Server> m_server;
+	
+	void SetUp() override {
+		m_server = std::make_unique<Server>();
+	}
+};
+
+TEST_F(ServerTest, TestThatServerCanDoX) {
+	// TODO write a test
 }
