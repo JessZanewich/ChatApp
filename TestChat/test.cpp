09dba12bcc520d90532d31b8b877f411fc082559
MostlyArmless <mike.j.thiem@gmail.com>
2019-04-15 20:19:08 -0600

    Created new test project and moved files to it. Old one broke after switching to C++17

---
+++ TestChat/test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "../BackEnd/Server.h"
+
+class ServerTest : public ::testing::Test {
+protected:
+	std::unique_ptr<Server> m_server;
+
+	void SetUp() override {
+		m_server = std::make_unique<Server>();
+	}
+};
+
+TEST_F(ServerTest, TestThatServerCanDoX) {
+	// TODO write a test
+
+}
--- TestChatApp/MockClient.cpp
+++
@@ -1,2 +0,0 @@
-#include "MockClient.h"
-
