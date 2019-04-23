544d035b0b095a6ac7cc02b772b057421504524d
MostlyArmless <mike.j.thiem@gmail.com>
2019-04-22 20:05:57 -0600

    installed packages and set up barebones server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,22 @@
-// Hello World app adapted from https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+// Reqs for a basic server
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+// import * as bcrypt from 'bcrypt-nodejs'; //! Don't need this yet
+// import * as knex from 'knex'; //! Don't need this yet
 
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+import * as jsonfile from 'jsonfile';
+
+async function main() {
+    // Configure the express server
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(cors());
+    
+    // Hook up the routes to the controllers
+    app.get('/', (req, res) => {
+        res.json('Hello this page exists now.');
+    });
 }
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-let user = new Student("Jane", "M.", "User");
-
-console.log(greeter(user));
+main();
