92028e4fe6750677f1d4050f10d21373a92f626a
MostlyArmless <mike.j.thiem@gmail.com>
2019-05-02 23:11:41 -0600

    IMessage.time now is Date instead of string

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,6 @@
-export interface Message {
+export interface IMessage {
     sender: string; // GUID for the user which sent the message
-    time: string; // ISO 8601 time string, e.g. "2019-04-14T00:21:31+00:00"
+    time: Date; // ISO 8601 time string, e.g. "2019-04-14T00:21:31+00:00"
     content: string; // string for the message content. should be sanitized by client before sending
     chatroom: string; // GUID for chat room
 }
