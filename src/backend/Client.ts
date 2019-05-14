7fb24a3f627d7f07279bc98767f71113d4d83d2a
MostlyArmless <mike.j.thiem@gmail.com>
2019-05-13 19:46:06 -0600

    Added Client class

--- src/backend/Client.ts
+++ src/backend/Client.ts
@@ -1,5 +1,13 @@
+import * as WebSocket from 'ws';
+import { IBaseMessage } from '../interfaces';
+
 export class Client {
     clientId: number;
     ws: WebSocket;
     chatrooms: string[];
+    messageHistory: IBaseMessage[];
+
+    constructor(ws: WebSocket) {
+        this.ws = ws;
+    }
 }
